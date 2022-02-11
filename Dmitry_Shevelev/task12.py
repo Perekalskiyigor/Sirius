e554649addcf18ac49e19d4037ac8c9671658232
Gimid934 <95129390+Gimid934@users.noreply.github.com>
2022-02-11 23:31:33 +0300

    Update task12.py

--- Dmitry_Shevelev/task12.py
+++ Dmitry_Shevelev/task12.py
@@ -1,5 +1,5 @@
 words: list = []
 while len(words) < 3:
-    words.append(input(">"))
+    words.append(input(">"))  # Считывание и добавление в список
 for c in words:
-    print(c)
+    print(c)  # Вывод строк по очедеди
