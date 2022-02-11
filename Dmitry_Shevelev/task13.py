e9397fb976ab16e2fec2a6fa37d4e0104dd133b4
Gimid934 <95129390+Gimid934@users.noreply.github.com>
2022-02-11 23:33:28 +0300

    Update task13.py

--- Dmitry_Shevelev/task13.py
+++ Dmitry_Shevelev/task13.py
@@ -1,4 +1,4 @@
 line1: str = input(">")
 line2: str = input(">")
-line3: str = input(">")
-print(line3+"\n"+line2+"\n"+line1)
+line3: str = input(">")  # Считываем строки
+print(line3+"\n"+line2+"\n"+line1)  # Выводим их в обратном порядке
